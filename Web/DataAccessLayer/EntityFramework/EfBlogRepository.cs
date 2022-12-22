5ac9038ef49d867cf2c9cce40c1a31d5369fa573
YusufCihanGedik <83451856+YusufCihanGedik@users.noreply.github.com>
2022-12-22 20:40:18 +0300

    Commit4
    Blog sayfası backend ve sayfa tasarımı

--- Web/DataAccessLayer/EntityFramework/EfBlogRepository.cs
+++ Web/DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -1,7 +1,9 @@
 ﻿
 using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositores;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,16 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
-    {
-    }
+	public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
+	{
+		public List<Blog> GetListWithCategory()
+		{
+			using (var c = new Context())
+			{
+				return c.Blogs.Include(x => x.Category).ToList();
+
+			}
+		}
+	}
+
 }
