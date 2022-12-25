130ab9bd86402018723badeaadd3c6b5b6b713a4
YusufCihanGedik <83451856+YusufCihanGedik@users.noreply.github.com>
2022-12-25 23:56:54 +0300

    Comment6
    Componentler İle Yorum Satırları Düzenleme

---
+++ Web/Web/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commnetValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID=1,
+					UserName="Yusf"
+				},
+				new UserComment
+				{
+					ID=2,
+					UserName="Yuaasf"
+				},
+				new UserComment
+				{
+					ID=3,
+					UserName="Yussf"
+				}
+			};
+			return View(commnetValues);
+		}
+	}
+}
