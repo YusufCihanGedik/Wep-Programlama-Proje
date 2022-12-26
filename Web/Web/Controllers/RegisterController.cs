b02ed325d9627d5c3d57f298fc34edf9f0089c79
YusufCihanGedik <83451856+YusufCihanGedik@users.noreply.github.com>
2022-12-26 21:44:28 +0300

    Commit7
    Commit7 Blog sayfasında kullanıcı kayıt işlemlerinin düzenlenmesi

---
+++ Web/Web/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+	
+	public class RegisterController : Controller
+	{
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			p.WriteStatus = true;
+			p.WriterAbout = "Deneme Test";
+			wm.WriterAdd(p);
+			return RedirectToAction("Index","Blog");
+		}
+	}
+}
